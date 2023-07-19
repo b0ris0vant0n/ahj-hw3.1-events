408a82542497290f9620ad213dc79bd171ed6352
Anton Borisov <borisov2011@gmail.com>
2023-07-19 11:35:12 +0300

    first commit

--- src/js/user-click.js
+++ src/js/user-click.js
@@ -7,16 +7,14 @@ export default function userClick(position) {
 
     let userMisses = document.querySelector('.user-got-misses__number')
 
-    userMisses = 0
 
     for (const playingItem of playingItemsCollection) {
       playingItem.addEventListener('click', (e) => {
           if (e.target.className === 'icon') {
             userPoints.textContent = +userPoints.textContent + 1
           } else {
-            userMisses++
             userMisses.textContent = +userMisses.textContent + 1
-            if (userMisses >= 5) {
+            if (+userMisses.textContent >= 5) {
                 gameOver();
                 clearTimeout(position);
             }
